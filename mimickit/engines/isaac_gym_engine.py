df4912bcf575e72d66b69559d0e6e35a625f4eda
Luckykantnayak <nayakluckykant01@gmail.com>
2025-12-24 01:59:03 -0500

    Go2W Specific Params are set

--- mimickit/engines/isaac_gym_engine.py
+++ mimickit/engines/isaac_gym_engine.py
@@ -601,7 +601,7 @@ class IsaacGymEngine(engine.Engine):
         sim_params.physx.max_depenetration_velocity = 10.0
         sim_params.physx.default_buffer_size_multiplier = 10.0
         sim_params.physx.num_subscenes = 0
-        sim_params.physx.max_gpu_contact_pairs = 8 * 1024 * 1024
+        sim_params.physx.max_gpu_contact_pairs = 20 * 1024 * 1024
         
         sim_params.flex.num_inner_iterations = 10
         sim_params.flex.warm_start = 0.25
